3a401740c22e72e5e5aa05cfe88d297148b9a666
DoomGuy1818 <zasdqs745@gmail.com>
2023-09-06 09:29:43 +0300

    add lec2

---
+++ lesson2/secondLesson.py
@@ -0,0 +1,12 @@
+a = 1
+b = 'b'
+c = True
+d = None
+
+_list1 : list = [a, b, c, d]
+
+_set1 : set = set([a, b, c, d])
+_set2: set = {a,b,c,d}
+_set3: set ={'m', 'i', 's', 's', 'i', 'p', 'p', 'i'}
+_set4: set = {'m', 'a', 'm', 'a'}
+print(_set3)

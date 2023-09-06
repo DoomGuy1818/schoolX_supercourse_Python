35aeafb4e39ca108b60ad1fb31bc1bde251dd6d4
Akhtem Musledinov <zasdqs745@gmail.com>
2023-09-06 21:25:58 +0300

    Done homework and some commit

---
+++ lesson2/2.3.py
@@ -0,0 +1,43 @@
+number: list = [
+    1,2,3,4,5,6,7,8,9
+]
+
+# for n in number:
+#     if n % 3 == 0:
+#         print(f"число - {n}")
+    
+#     if n % 3 != 0:
+#         print(f'Число {n} не кратно 3')
+
+# for n in number:
+#     if n%7 == 0:
+#         print(f"{n}")
+    
+#     elif n%5 == 0:
+#         print(f"{n}")
+        
+#     else:
+#         print("-")
+
+
+# word = input('Введите слово:')
+# vowel ='aeiouy'
+
+# vowel_count: int = 0
+
+# for char in word:
+#     # print(char)
+#     if char in vowel:
+#         vowel_count += 1
+
+# print(vowel_count)
+
+n: int = int(input('N: '))
+summ : int = 0
+
+for i in range(n+1):
+    # if i % 3 == 0 or i % 5 == 0:
+    #     summ += i
+    print(i)
+
+    

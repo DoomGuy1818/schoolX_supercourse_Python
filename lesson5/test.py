cd833ae91cbf6f184a4006bac0ac9cdb7a78216e
Akhtem Musledinov <zasdqs745@gmail.com>
2023-09-22 20:47:56 +0300

    update

--- lesson5/test.py
+++ lesson5/test.py
@@ -4,7 +4,7 @@ b: list[int] = [0,0,1,0,0,1]
 answer = []
 
 answer = [
-    val*b[i] 
+    val+b[i] 
     for i, val in enumerate(a)
 ]
 # answer = [i for i in a] # более простой вариант записи массива
@@ -15,7 +15,7 @@ for i, val in enumerate(a):
     print(f'value={val}\n')
 
 def mask_list(array: list[int], mask: list[int]) -> list[int]:
-    return [val * mask[i] * .5 for i, val in enumerate(array)]
+    return [val * mask[i] for i, val in enumerate(array)]
 
 def test_mask_list():
     assert mask_list([1,2,3], [1,0,0]) == [1, 0, 0] 
